bdc4839d8bd162392db5455b024ea5ad60879848
Isadora-Santos1 <isadora.sass@gmail.com>
2021-05-26 12:53:48 -0400

    Add project files.

---
+++ DotNetDrinks/Controllers/BrandsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetDrinks.Controllers
+{
+    public class BrandsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: /Brands/Details
+        public IActionResult Details(string name)
+        {
+            ViewBag.name = name;
+            return View();
+        }
+    }
+}
---
+++ DotNetDrinks/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetDrinks.Models
+{
+    public class Brand
+    {
+        //in ASP.NET, key fields shoudl always be called either Id ir{Model}Id
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearFounded { get; set; }
+    }
+}
